f1bde42315108f6fa4be38fe653c97eb5edad63c
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-11-29 16:15:12 -0600

    Create band_generator.py

---
+++ 100 Days of Code/Day-1/band_generator.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to Troy's Band Name Generator!")
+#2. Ask the user for the city that they grew up in.
+city = input("Please enter the name of the city you grew up in:\n")
+#3. Ask the user for the name of a pet.
+pet = input("Please enter the name of one of your pets:\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("You could call your band the " + city + " " + pet + "'s")
