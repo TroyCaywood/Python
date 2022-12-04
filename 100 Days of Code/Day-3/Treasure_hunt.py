751bb93478cf5eb7cc63f273be2c5a064b09a7e8
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-04 12:25:34 -0600

    Create Treasure_hunt.py

---
+++ 100 Days of Code/Day-3/Treasure_hunt.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+answer1 = input("You have come to crossroad. Do you go left or right?")
+
+if answer1.lower() == "left":
+    print("Game Over.")
+elif answer1.lower() == "right":
+    answer2 = input("You are in a large room with a golden door. Do you open the door? Yes or No.")
+    if answer2.lower() == "yes":
+        print("Hah! That was a trick! The door falls on you crushing you. Game Over.")
+    elif answer2.lower() == "no":
+        answer3 = input("You are now in front of a large lake. A giant talking bear appears and asks you to choose between three shells. Do you choose the first, the second, or the third?")
+        if answer3.lower() == "first":
+            print("The bear doesn't like that answer and eats you. Game over.")
+        elif answer3.lower() == "third":
+            print("That is not the right answer. Game Over!")
+        elif answer3.lower() == "second":
+            print("This pleases the bear. He gives you a treasure chest and flies off into the sunset. You win!")
