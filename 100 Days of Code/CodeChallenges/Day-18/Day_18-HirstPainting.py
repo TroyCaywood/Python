6f57bc011fa85924567bfee3c63c6e11eb40f173
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-22 08:08:54 -0600

    Update Day_18-HirstPainting.py

--- 100 Days of Code/CodeChallenges/Day-18/Day_18-HirstPainting.py
+++ 100 Days of Code/CodeChallenges/Day-18/Day_18-HirstPainting.py
@@ -1,5 +1,8 @@
 import random
 import turtle as t
+
+# Import colorgram and grab colors from a hirst painting image, then run through the list of colors and create a tuple
+# for each color and put them in the color list - only needed once to pull colors
 # import colorgram
 # colors = colorgram.extract('image.jpg', 30)
 # for color in colors:
@@ -13,26 +16,34 @@ import turtle as t
 
 
 color_list = [(26, 109, 164), (194, 38, 81), (237, 161, 50), (234, 215, 86), (227, 237, 229), (222, 137, 176),
-               (143, 109, 57), (101, 197, 219), (206, 166, 29), (21, 58, 132), (212, 75, 91), (238, 89, 49),
-               (141, 208, 227), (119, 192, 141), (6, 160, 87), (4, 186, 179), (106, 108, 198), (136, 29, 72),
-               (98, 51, 37), (25, 153, 211), (228, 168, 188), (153, 213, 195), (173, 186, 221), (234, 174, 162),
-               (30, 91, 95), (87, 47, 34), (34, 46, 84)]
+              (143, 109, 57), (101, 197, 219), (206, 166, 29), (21, 58, 132), (212, 75, 91), (238, 89, 49),
+              (141, 208, 227), (119, 192, 141), (6, 160, 87), (4, 186, 179), (106, 108, 198), (136, 29, 72),
+              (98, 51, 37), (25, 153, 211), (228, 168, 188), (153, 213, 195), (173, 186, 221), (234, 174, 162),
+              (30, 91, 95), (87, 47, 34), (34, 46, 84)]
 # 10 x 10 spots,  20 size 50 paces space. filled dots
 tom = t.Turtle()
 tom.pensize(20)
 t.colormode(255)
 dot_count = 0
+random_color = random.choice(color_list)
+
 
 def move_turtle():
-    tom.pendown()
-    tom.forward(0)
+
+    for _ in range(10):
+        tom.pencolor(random.choice(color_list))
+        tom.pendown()
+        tom.forward(0)
+        tom.penup()
+        tom.forward(50)
     tom.penup()
+    tom.left(90)
     tom.forward(50)
-    
+    tom.left(90)
+    tom.forward(500)
+    tom.right(180)
 
-
-while dot_count < 10:
+for _ in range(10):
     move_turtle()
-    dot_count += 1
 
 t.exitonclick()
