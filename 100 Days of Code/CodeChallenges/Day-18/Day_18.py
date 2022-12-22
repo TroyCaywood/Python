3359cc39ca10a817a554075b7ce28f5a7bfc22ca
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-21 22:46:18 -0600

    Update Day_18.py

--- 100 Days of Code/CodeChallenges/Day-18/Day_18.py
+++ 100 Days of Code/CodeChallenges/Day-18/Day_18.py
@@ -1,31 +1,38 @@
-from turtle import Turtle, Screen
 import random
+import turtle as t
+# import colorgram
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     color_tuple = (r, g, b)
+#     color_list.append(color_tuple)
+#
+# print(color_list)
 
-# Create turtle object
-tim = Turtle()
 
-# Change turtle shape to "turtle" shape
-tim.shape("turtle")
+color_list = [(26, 109, 164), (194, 38, 81), (237, 161, 50), (234, 215, 86), (227, 237, 229), (222, 137, 176),
+               (143, 109, 57), (101, 197, 219), (206, 166, 29), (21, 58, 132), (212, 75, 91), (238, 89, 49),
+               (141, 208, 227), (119, 192, 141), (6, 160, 87), (4, 186, 179), (106, 108, 198), (136, 29, 72),
+               (98, 51, 37), (25, 153, 211), (228, 168, 188), (153, 213, 195), (173, 186, 221), (234, 174, 162),
+               (30, 91, 95), (87, 47, 34), (34, 46, 84)]
+# 10 x 10 spots,  20 size 50 paces space. filled dots
+tom = t.Turtle()
+tom.pensize(20)
+t.colormode(255)
+dot_count = 0
 
-# Change turtle color to plum
+def move_turtle():
+    tom.pendown()
+    tom.forward(0)
+    tom.penup()
+    tom.forward(50)
+    
 
-tim.pencolor(
-        float(random.randint(1, 255)),
-        float(random.randint(1, 255)),
-        float(random.randint(1, 255))
-        )
-print(type(timmy))
 
-# Move turtle forwards 100 and turn right 4 times
-for i in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+while dot_count < 10:
+    move_turtle()
+    dot_count += 1
 
-# Create screen object
-screen = Screen()
-
-# Set screen object to display on until clicked
-screen.colormode(255)
-screen.exitonclick()
+t.exitonclick()
