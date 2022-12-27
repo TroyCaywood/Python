f3a7bfb5919ea820c4aaf474f598b5737aa7cd5b
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-27 16:50:55 -0600

    Update main.py

--- 100 Days of Code/CodeChallenges/Day-22/main.py
+++ 100 Days of Code/CodeChallenges/Day-22/main.py
@@ -5,13 +5,23 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PingPong")
-# screen.tracer(0)
+screen.tracer(0)
 
 right_paddle = Paddle()
-right_paddle.goto(350, 0)
+right_paddle.new_paddle.setposition(350, 0)
+
+left_paddle = Paddle()
+left_paddle.new_paddle.setposition(-350, 0)
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
+screen.onkey(left_paddle.up, "w")
+screen.onkey(left_paddle.down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
