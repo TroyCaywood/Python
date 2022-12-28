7e5eda9e67e3077b0ccb789705be9d2c433bfdd2
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-28 16:31:56 -0600

    Update car_manager.py

--- 100 Days of Code/CodeChallenges/Day-23/car_manager.py
+++ 100 Days of Code/CodeChallenges/Day-23/car_manager.py
@@ -16,16 +16,19 @@ class CarManager(Turtle):
         self.car_speed = MOVE_INCREMENT
 
     def create_car(self):
+        # Space out creation of cars by only creating cars if randint picks 1
         random_chance = random.randint(1, 6)
         if random_chance == 1:
             new_car = Turtle("square")
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.penup()
-            new_car.color(random.choice(COLORS))
+            new_car.color(random.choice(COLORS)
             random_y = random.randint(-250, 250)
+            # Place car on random Y position
             new_car.goto(300, random_y)
             self.all_cars.append(new_car)
-
+    
+    # Move cars
     def car_move(self):
         for car in self.all_cars:
             car.moving = car.xcor() - self.car_speed
