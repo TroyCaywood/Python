f185f77ce3e4f8a8a19122cf92c588fecc5c7615
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-28 09:58:46 -0600

    Update main.py

--- 100 Days of Code/CodeChallenges/Day-22/main.py
+++ 100 Days of Code/CodeChallenges/Day-22/main.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,11 +10,12 @@ screen.bgcolor("black")
 screen.title("PingPong")
 screen.tracer(0)
 
-right_paddle = Paddle()
-right_paddle.new_paddle.setposition(350, 0)
+scoreboard = Scoreboard()
 
-left_paddle = Paddle()
-left_paddle.new_paddle.setposition(-350, 0)
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
@@ -21,7 +25,27 @@ screen.onkey(left_paddle.down, "s")
 
 game_is_on = True
 
+
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+    # Detect ball collisions with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect if right paddle misses
+    if ball.xcor() > 380:
+        scoreboard.l_increase_score()
+        ball.reset_position()
+
+    # Detect if left paddle misses:
+    if ball.xcor() < -380:
+        scoreboard.r_increase_score()
+        ball.reset_position()
 
 screen.exitonclick()
