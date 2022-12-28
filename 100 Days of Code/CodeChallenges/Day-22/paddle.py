b8bdb17ad702d33e33ffc561a43528c8a2048952
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-28 10:05:55 -0600

    Update paddle.py

--- 100 Days of Code/CodeChallenges/Day-22/paddle.py
+++ 100 Days of Code/CodeChallenges/Day-22/paddle.py
@@ -9,11 +9,13 @@ class Paddle(Turtle):
         self.turtlesize(stretch_wid=5, stretch_len=1, outline=None)
         self.penup()
         self.goto(position)
-
+    
+    # Define up movement method
     def up(self):
         self.new_y = self.ycor() + 20
         self.goto(self.xcor(), self.new_y)
-
+    
+    # Define down movement method
     def down(self):
         self.new_y = self.ycor() - 20
         self.goto(self.xcor(), self.new_y)
