64bdc0ad4485661dea840f1ebcc1f35daa34a8f9
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2022-12-29 22:10:20 -0600

    Remove unnecessary indent in main.py

--- 100 Days of Code/CodeChallenges/Day-24/main.py
+++ 100 Days of Code/CodeChallenges/Day-24/main.py
@@ -11,6 +11,6 @@ with open("./Input/Names/invited_names.txt", mode="r") as invited_names:
             # Replace [name] section in starting_letter with current name from name list and strip return after name
             replace_name = read_letter.replace("[name]", name.rstrip("\r\n"))
             # Open new letter in append mode using name variable
-            with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="a+") as output_letter:
-                # Write replacement name variable to new letter
-                output_letter.write(replace_name)
+        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="a+") as output_letter:
+            # Write replacement name variable to new letter
+            output_letter.write(replace_name)
