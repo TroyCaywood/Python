6450867e27ca5211bb01785886783ce040e0be73
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2023-01-06 15:04:44 -0600

    Update main.py

--- 100 Days of Code/CodeChallenges/Day-28/main.py
+++ 100 Days of Code/CodeChallenges/Day-28/main.py
@@ -25,9 +25,23 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+timer_label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
 
+start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"))
+start_button.grid(column=0, row=2)
 
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button.grid(column=2, row=2)
 
+checkmark = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+checkmark.grid(column=1, row=3)
+
+# grid
+# ✓
+# fg=Green
 window.mainloop()
+
+# 3(2) columns 4(3) rows
