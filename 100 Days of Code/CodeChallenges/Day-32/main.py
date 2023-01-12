8c16288f2a39e8b202ed628fec726e9e9943d46c
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2023-01-11 21:56:48 -0600

    update main.py

--- 100 Days of Code/CodeChallenges/Day-32/main.py
+++ 100 Days of Code/CodeChallenges/Day-32/main.py
@@ -15,10 +15,10 @@ print(data)
 birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
 
 # If today's date is in birthday's dict send letter
-if (today_month, today_day) in birthdays_dict:
+if today in birthdays_dict:
 
     # Get name for current day
-    name = birthdays_dict[(today_month, today_day)]["name"]
+    name = birthdays_dict[today]["name"]
     letter_number = random.randint(1, 3)
     letter = f"./letter_templates/letter_{letter_number}.txt"
 
@@ -30,7 +30,7 @@ if (today_month, today_day) in birthdays_dict:
     # Send email
     my_email = "someemail@gmail.com"
     password = "somepassword"
-    to_address = birthdays_dict[(today_month, today_day)]["email"]
+    to_address = birthdays_dict[today]["email"]
 
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
