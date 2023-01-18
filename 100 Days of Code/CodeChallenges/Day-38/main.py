b9d994676672dd10963ae14c530833cd4a1fcc13
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2023-01-18 15:21:29 -0600

    Create Day-38 main.py

---
+++ 100 Days of Code/CodeChallenges/Day-38/main.py
@@ -0,0 +1,55 @@
+import requests
+import datetime
+import os
+
+APP_KEY = os.environ["APP_KEY"]
+APP_ID = os.environ["APP_ID"]
+USER_ID = os.environ["USER_ID"]
+
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
+SHEET_ENDPOINT = os.environ["SHEET_ENDPOINT"]
+
+HEADERS = {
+    "x-app-key": APP_KEY,
+    "x-app-id": APP_ID,
+    "x-remote-user-id": USER_ID
+}
+
+exercise_json = {
+    "query": f"{input('What exercises did you do today and for how long? ')}",
+    "gender": "male",
+    "weight_kg": 73.03,
+    "height_cm": 177.8,
+    "age": 38
+}
+
+exercise_post_response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", headers=HEADERS, json=exercise_json)
+exercise_post_response.raise_for_status()
+result = exercise_post_response.json()
+print(result)
+
+# Create date and time strings
+today_now = datetime.datetime.now()
+today_time = today_now.strftime("%H:%M")
+today_now_str = today_now.strftime("%d/%m/%Y")
+
+# Create parameters for sheetly post from each exercise submitted
+for exercise in result["exercises"]:
+    sheetly_params = {
+        "workout": {
+            "date": today_now_str,
+            "time": today_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+
+    }
+
+    headers = {
+        "Authorization": BEARER_TOKEN
+    }
+
+    sheetly_response = requests.post(url=SHEET_ENDPOINT, json=sheetly_params, headers=headers)
+    sheetly_response.raise_for_status()
+    print(sheetly_response.text)
