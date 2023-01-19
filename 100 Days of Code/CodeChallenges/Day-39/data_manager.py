eea995f998853cd9bef409ce2e71aa92f8384477
Troy Caywood <52113778+TroyCaywood@users.noreply.github.com>
2023-01-19 17:04:41 -0600

    Update main.py

--- 100 Days of Code/CodeChallenges/Day-39/data_manager.py
+++ 100 Days of Code/CodeChallenges/Day-39/data_manager.py
@@ -1,3 +1,37 @@
+from pprint import pprint
+import requests
+
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_GET_URL)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # 3. Try importing pretty print and printing the data out again using pprint() to see it formatted.
+        # pprint(data)
+        return self.destination_data
+
+    # 6. In the DataManager Class make a PUT request and use the row id from sheet_data
+    # to update the Google Sheet with the IATA codes. (Do this using code).
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+
+            response = requests.put(
+                url=f"{SHEETY_GET_URL}/{city['id']}",
+                json=new_data,
+
+            )
+            print(response.text)
+
+
+
--- 100 Days of Code/CodeChallenges/Day-39/flight_search.py
+++ 100 Days of Code/CodeChallenges/Day-39/flight_search.py
@@ -1,3 +1,40 @@
+import requests
+import data_manager
+from pprint import pprint
+
+
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        json = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        headers = {
+            "apikey": KIWI_API_KEY
+        }
+
+        response = requests.get(url=KIWI_LOCATIONS_ENDPOINT, headers=headers, json=json)
+        data = response.json()
+
+        code = data["code"]
+        return code
+
+
+# json = {
+#     "term": "Wichita",
+#     "location_types": "city"
+# }
+# headers = {
+#     "apikey": KIWI_API_KEY
+# }
+#
+# response = requests.get(url=KIWI_LOCATIONS_ENDPOINT, headers=headers, params=json)
+# data = response.json()
+# pprint(data)
+
+
